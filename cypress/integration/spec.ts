98a67711e51558b41bb6f57672099af09835ae27
yangtopia <yangtopia@gmail.com>
2021-08-12 17:35:08 +0900

    chore: test modified

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -2,6 +2,6 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
     cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
+    // cy.contains('sandbox app is running!')
   })
 })
